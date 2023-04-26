c7289f241091d0dcf536f343e2e9c71aff86038e
Matt DeKrey <mattdekrey@gmail.com>
2023-04-26 10:54:33 -0500

    Use more common function to check property

--- src/internals/utils.ts
+++ src/internals/utils.ts
@@ -1,5 +1,7 @@
 import { type Atom } from 'jotai';
 
 export function isSignal(target: unknown): target is Atom<unknown> {
-	return (target || false) && Object.hasOwn(target, 'read');
+	return (
+		(target || false) && Object.prototype.hasOwnProperty.call(target, 'read')
+	);
 }
