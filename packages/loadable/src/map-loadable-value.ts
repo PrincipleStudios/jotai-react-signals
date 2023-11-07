03ef91a66a0dd1032190635b0a3e720da94f1582
Matt DeKrey <matt.dekrey@principlestudios.com>
2023-11-07 07:34:28 -1000

    Correct lint settings

--- packages/loadable/src/map-loadable-value.ts
+++ packages/loadable/src/map-loadable-value.ts
@@ -1,10 +1,20 @@
 import { mapLoadable } from './map-loadable';
-import { makeError, makeIdle, makeLoaded, makeLoading, Loadable } from './loadable';
+import {
+	makeError,
+	makeIdle,
+	makeLoaded,
+	makeLoading,
+	Loadable,
+} from './loadable';
 
-export function mapLoadableValue<T, TResult>(target: Loadable<T>, convert: (input: T) => TResult): Loadable<TResult> {
+export function mapLoadableValue<T, TResult>(
+	target: Loadable<T>,
+	convert: (input: T) => TResult
+): Loadable<TResult> {
 	return mapLoadable<T, Loadable<TResult>>(target, {
 		idle: makeIdle,
-		loading: (value) => makeLoading(value === undefined ? undefined : convert(value)),
+		loading: (value) =>
+			makeLoading(value === undefined ? undefined : convert(value)),
 		loaded: (value) => makeLoaded(convert(value)),
 		error: makeError,
 	});
