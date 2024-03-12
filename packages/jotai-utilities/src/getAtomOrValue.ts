48abe5842265b4e3be225e6f902ef3f8a71f7fc2
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-03-12 09:50:56 -0500

    Rename function to match prior example, update docs

--- packages/jotai-utilities/src/getAtomOrValue.ts
+++
@@ -1,7 +0,0 @@
-import type { Atom, Getter } from 'jotai';
-import { isAtom } from './isAtom';
-
-export function getAtomOrValue<T>(get: Getter, target: Atom<T> | T) {
-	if (isAtom(target)) return get(target);
-	return target;
-}
