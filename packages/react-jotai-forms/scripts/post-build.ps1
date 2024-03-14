3f59e6719791ffb2cbe4e1f394beeba1ac5429cd
Matt DeKrey <matt.dekrey@principlestudios.com>
2024-03-14 14:24:12 -0500

    Build simplification

--- packages/react-jotai-forms/scripts/post-build.ps1
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env pwsh
-
-Push-Location "$PSScriptRoot/.."
-try {
-	# '{ "type": "commonjs" }' | Out-File dist/cjs/package.json
-	# '{ "type": "module" }' | Out-File dist/mjs/package.json
-} finally {
-	Pop-Location
-}
